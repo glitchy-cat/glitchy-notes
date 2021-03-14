0e061e5e5de853106a2fbc85f95d9ccf86f1cced
Jimmy Bull <jimmybull18@gmail.com>
2021-03-14 15:39:27 -0400

    Added some styling to additem component

--- components/additem.jsx
+++ components/additem.jsx
@@ -1,36 +1,45 @@
-import React, {useState, useEffect} from 'react';
+import React, { useState, useEffect } from "react";
 
 function Addtodo(props) {
+  const [inputText, setInputText] = useState("");
+  const [list, setList] = useState([]);
 
-    const [inputText, setInputText] = useState('');
-    const [list, setList] = useState ([]);
-    
-    const onInputChange = async (e) => {
-        console.log(e.currentTarget.value);
-        setInputText(e.currentTarget.value);
-    }
+  const onInputChange = async (e) => {
+    console.log(e.currentTarget.value);
+    setInputText(e.currentTarget.value);
+  };
 
-    const addItem = async () => {
-        setList([...list, inputText]);
-    };
+  const addItem = async () => {
+    setList([...list, inputText]);
+  };
 
-    return(
-        
-        <div>
-                <input type="text" value={inputText} onChange={onInputChange} />
-                <button onClick={addItem}>Enter task</button>
-
-        <ul>
-            {list.map((item, idx) => {
-                return <li key={idx}> {item} </li>                
-            })}
-        </ul>
-
-
+  return (
+    //todos page
+    <div className={"m-10 p-5 border"}>
+      <div className={"grid grid-cols-2 gap-4"}>
+        <div className={"border p-2"}>
+          <input
+            className={"border w-1/2 p-1 border border-gray-300 rounded"}
+            type="text"
+            placeholder="Enter tasks"
+            value={inputText}
+            onChange={onInputChange}
+          />
         </div>
-    )
+        <div className={"border p-2"}>
+          <button className={'p-1 rounded pl-3 pr-3 cursor-pointer text-white text-xl bg-blue-500'} onClick={addItem}>
+            Add
+          </button>
+        </div>
+      </div>
 
+      <ul>
+        {list.map((item, idx) => {
+          return <li key={idx}> {item} </li>;
+        })}
+      </ul>
+    </div>
+  );
 }
-
 
 export default Addtodo;
--- pages/index.js
+++ pages/index.js
@@ -1,16 +1,9 @@
-import Head from 'next/head'
-import styles from '../styles/Home.module.css'
-import Addtodo from '../components/additem'
+import Addtodo from '../components/additem';
 
 export default function Home() {
   return (
-    <div className={styles.container}>
-
+    <div>
       <Addtodo />
-     
-      <footer className={styles.footer}>
-        
-      </footer>
     </div>
   )
 }
