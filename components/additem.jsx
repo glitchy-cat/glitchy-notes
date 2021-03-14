1129d5735879c8613299443dd0c6d134a72cb0aa
Jimmy Bull <jimmybull18@gmail.com>
2021-03-14 18:26:14 -0400

    improved item list ui

--- components/additem.jsx
+++ components/additem.jsx
@@ -17,20 +17,31 @@ function Addtodo(props) {
     //todos page
     <div className={"m-10 p-5 border"}>
       <div className={"grid grid-cols-2 gap-4"}>
-        <div className={"border p-2"}>
+        <div className={"p-2"}>
           <input
-            className={"border w-1/2 p-1 border border-gray-300 rounded"}
+            className={"border w-1/2 p-1 border-gray-300 rounded"}
             type="text"
             placeholder="Enter tasks"
             value={inputText}
             onChange={onInputChange}
           />
         </div>
-        <div className={"border p-2"}>
+        <div className={"p-2"}>
           <button className={'p-1 rounded pl-3 pr-3 cursor-pointer text-white text-xl bg-blue-500'} onClick={addItem}>
             Add
           </button>
         </div>
+      </div>
+
+      <br/>
+      <br/>
+      <div className={'bg-gray-100 p-2 rounded'}>
+        <ul>
+          <li className={'border-b-2 border-cool-gray-300'}>
+            <input type="checkbox"/>&nbsp;&nbsp;
+            <span className={'text-gray-700 font-semibold text-sm'}>Work on project</span>
+          </li>
+        </ul>
       </div>
 
       <ul>
@@ -38,6 +49,14 @@ function Addtodo(props) {
           return <li key={idx}> {item} </li>;
         })}
       </ul>
+      
+
+
+        
+
+
+
+
     </div>
   );
 }
